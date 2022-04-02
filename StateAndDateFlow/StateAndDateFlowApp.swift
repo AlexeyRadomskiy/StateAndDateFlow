d0b339385c6d23e65d1aec6ba12f5ef4bff3c609
Alexey <alexeyradomskiy98@gmail.com>
2022-04-02 18:58:46 +0300

    Initial Commit

---
+++ StateAndDateFlow/StateAndDateFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDateFlowApp.swift
+//  StateAndDateFlow
+//
+//  Created by Alexey on 02.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDateFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
